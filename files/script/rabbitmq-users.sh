0e6991ae4c59b0a438aeda0d1a100bdb6bd5230f
John Norwood <norwood.john.m@gmail.com>
2020-01-18 19:04:50 +0000

    feat: quoting everything in the rabbitmq script properly and adds management tags to new users

--- files/script/rabbitmq-users.sh
+++ files/script/rabbitmq-users.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-rabbitmqctl add_user mqadmin {{ admin_password }}
+rabbitmqctl add_user mqadmin '{{ admin_password }}'
 rabbitmqctl set_user_tags mqadmin administrator
 
 {% for v, users in vhosts.items() %}
-rabbitmqctl add_vhost {{ v }}
+rabbitmqctl add_vhost '{{ v }}'
 
   {% for u in users %}
-rabbitmqctl add_user {{ u["username"] }} {{ u["password"] }}
-rabbitmqctl set_permissions -p {{ v }} {{ u["username"] }} ".*" ".*" ".*"
+rabbitmqctl add_user '{{ u["username"] }}' '{{ u["password"] }}'
+rabbitmqctl set_permissions -p '{{ v }}' '{{ u["username"] }}' ".*" ".*" ".*"
+rabbitmqctl set_user_tags '{{ u["username"] }}' management
   {% endfor %}
 {% endfor %}
